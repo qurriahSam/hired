7bdbb2599c87407df6619068956ae6d9b9b92b99
qurriahsam <mugirethe1@gmail.com>
2022-08-19 21:39:59 +0300

    add padding and center navlinks

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,21 +9,24 @@ const TopNav = () => {
       <Container>
         <Navbar.Brand href="#home" className="fw-bold fs-4">
           Hired
+          <span style={{ color: "rgb(12, 110, 253)" }}>.</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="mx-auto">
             <Nav.Link href="#home" className="fw-semibold">
               Home
             </Nav.Link>
-            <Nav.Link href="#link">Reviews</Nav.Link>
-            <Nav.Link href="#link" className="md:ps-5">
+            <Nav.Link href="#link" className="fw-semibold ms-lg-5">
+              Reviews
+            </Nav.Link>
+            <Nav.Link href="#link2" className="fw-semibold ms-lg-5 pb-3 pb-lg-0">
               Post a Job
             </Nav.Link>
           </Nav>
-          <p className="pt-3 pe-4">
-            <a href="#">Login</a>
-          </p>
+          <Nav.Link href="#link3" className="fw-semibold pe-3 pb-3 pb-lg-0">
+            Login
+          </Nav.Link>
           <Button className="px-5 fw-bold">Sign Up</Button>
         </Navbar.Collapse>
       </Container>
