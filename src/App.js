f6782d11afab0c7392acdf0c682fbf1b3f708d23
qurriahsam <mugirethe1@gmail.com>
2022-08-19 11:39:45 +0300

    add fontawesome icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-capitalize text-decoration-underline text-center fst-italic">hired</h1>
+    <div className="d-flex">
+      <h1 className="text-capitalize text-decoration-underline fst-italic">hired</h1>
+      <FontAwesomeIcon icon={faCoffee} className="p-3" />
     </div>
   );
 }
