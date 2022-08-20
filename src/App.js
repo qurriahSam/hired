80d11c8639b809ffe9f58e4ef3efcac72fbd5e32
qurriahsam <mugirethe1@gmail.com>
2022-08-20 22:35:53 +0300

    import & display navbar and landingpage components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import LandingPage from "./components/landingpage/LandingPage";
+import TopNav from "./components/Navbar";
 
 function App() {
   return (
-    <div className="d-flex">
-      <h1 className="text-capitalize text-decoration-underline fst-italic">hired</h1>
-      <FontAwesomeIcon icon={faCoffee} className="p-3" />
-    </div>
+    <>
+      <TopNav />
+      <LandingPage />
+    </>
   );
 }
 
