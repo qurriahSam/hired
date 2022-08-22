64ba61141d6df21948762492646e904c809acd4f
Precious <preciousmaregwa1997@gmail.com>
2022-08-22 17:39:29 +0300

    add jobseeker card

--- src/components/manageAccounts/jobseekers.jsx
+++ src/components/manageAccounts/jobseekers.jsx
@@ -4,10 +4,15 @@ import Card from 'react-bootstrap/Card';
 
 function BasicExample() {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+    <Card className="p-3 text-center shadow mx-auto mb-3 mx-md-3" style={{ width: "18rem" }}>
+    <Card.Img
+      variant="top"
+      src={girlReviewImg}
+      className="rounded-circle mx-auto"
+      style={{ width: "6rem" }}
+    />
+    <Card.Body>
+        <Card.Title>JobSeeker Name</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
