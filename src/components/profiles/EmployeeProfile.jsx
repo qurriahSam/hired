4591004886e3827ad37a6100a75706633ab7dc14
desmond <desmond@desmonds-MacBook-Pro.local>
2022-08-23 20:17:13 +0300

    added resume section on the jobseeker portfolio with an image icone of a file

--- src/components/profiles/EmployeeProfile.jsx
+++ src/components/profiles/EmployeeProfile.jsx
@@ -3,6 +3,7 @@ import Card from 'react-bootstrap/Card';
 import girlReviewImg from "../../images/ladyreview.jpg";
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
+//import { faFile } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
@@ -81,12 +82,25 @@ function EmployeeProfile() {
           <p>In addition, i am also labeled as the “Mediator”. This means that I am more introverted than extroverted and very disorganized. I am also dedicated and hardworking in reaching my goals and visions. In addition, being a mediator means that I’m open and flexible, but on the flip side that also means that I can be too impractical and take things too personally.</p>
         </Card.Subtitle>
         </div>
+        </Card.Body>
+    </Card>
+
+    <Card className="shadow p-3 mb-5 bg-white rounded mx-auto"   style={{ width: '50em', height:"25rem", context:"center", marginTop: "7em"}}>
+    <FontAwesomeIcon icon={faPencil} className="mt-1" color="gray" fontSize={30} width="88rem"/>
+     
+      <Card.Body className='d-flex' style={{ width: '20rem',  height:"20rem"}} >
         
+        <div>
+          
+         <Card.Title style={{ width: '44rem',fontFamily:"georgia",fontSize:"25px"}}>Resume</Card.Title>
+         <Card.Img variant="center"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGa6mTw7RA9yXLKb4Tjx84pC0uxMbWGQAZWSlOqZo&s"
+            className="potrait mx-auto"
+            style={{ width: "15rem" }}></Card.Img>
+         
+        </div>
         
-        <Card.Text></Card.Text>
-        
-        
-      </Card.Body>
+        </Card.Body>
     </Card>
     
     </div>
