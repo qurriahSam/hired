7c1b8cbb2b68fac92beed8c05514cbea9d53ab4f
desmond <desmond@desmonds-MacBook-Pro.local>
2022-08-24 15:44:52 +0300

    jobseeker pending added to varification info

--- src/components/profiles/JobSeekerVarification.jsx
+++ src/components/profiles/JobSeekerVarification.jsx
@@ -47,6 +47,27 @@ function JobSeekerVarification() {
       </div>
       </Card.Body>
     </Card>
+
+    <Card className="shadow p-3 mb-5 bg-white rounded mx-auto"   style={{ width: '50em', height:"31rem",  marginTop: "7em"}}>
+      <Card.Title style={{ width: '15rem',fontFamily:"georgia",fontSize:"30px",marginLeft:"1%"}}>Company name</Card.Title>
+      <Card.Subtitle style={{ width: '25rem',fontFamily:"san-sarif",fontSize:"20px", marginTop:"1rem", marginLeft:"38rem", color:"blue"}}>Pending</Card.Subtitle>
+      <Card.Subtitle style={{ width: '25rem',fontFamily:"georgia",fontSize:"25px", marginTop:"1rem", marginLeft:"2rem"}}>Job description</Card.Subtitle>
+      <Card.Body >
+        <Card.Text>
+        Once the essential functions are defined, the employer can make a determination as to whether the functions are essential or marginal. The use of the term "essential function" should be part of the job description, and it should explicitly state how an individual is to perform the job. This will provide future guidance as to whether the job can be performed with or without an accommodation.Once the performance standard for a particular job has been made, essential functions of the position must be defined. This will provide a better avenue for evaluating Americans with Disabilities Act (ADA) accommodation requests. Defining the essential functions encompasses the following steps:
+        </Card.Text>
+        <div className='d-flex'>
+        <div style={{ width: '15rem',fontFamily:"georgia",fontSize:"30px",marginTop:"5rem", marginLeft:"35rem", }}>
+        <div className="mb-2">
+    
+        <Button variant="danger" size="lm" active>
+          Withdraw
+        </Button>
+      </div>
+      </div>
+      </div>
+      </Card.Body>
+    </Card>
     </div>
 
     
