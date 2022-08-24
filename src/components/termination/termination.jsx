409e7ce213c3d89b4b79a7d4beae546444305013
Precious <preciousmaregwa1997@gmail.com>
2022-08-24 11:38:32 +0300

    add termination card

--- src/components/termination/termination.jsx
+++ src/components/termination/termination.jsx
@@ -2,3 +2,11 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import girlReviewImg from "../../images/ladyreview.jpg"
+
+function Jobseekers() {
+    return ( 
+      <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+          <div className="d-md-flex">
