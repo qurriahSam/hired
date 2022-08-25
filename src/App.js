9b90b6f74d96e860e6be2850e02e2008aae8a05f
qurriahsam <mugirethe1@gmail.com>
2022-08-25 12:42:19 +0300

    add signup form to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-import LandingPage from "./components/landingpage/LandingPage";
+//import LandingPage from "./components/landingpage/LandingPage";
 import TopNav from "./components/Navbar";
-
+import SignUpOrIn from "./components/signup/SignUpOrIn";
 function App() {
   return (
-    <>
+    <div>
       <TopNav />
-      <LandingPage />
-    </>
+      <SignUpOrIn />
+    </div>
   );
 }
 
