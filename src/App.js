884c44cd80add3f7ad68e7ccd0e5e9449ebce8a3
qurriahsam <mugirethe1@gmail.com>
2022-08-26 21:34:08 +0300

    create context to share user info between components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 //import Account from "./components/approveFiles/Jobseekers";
@@ -10,19 +10,26 @@ import SignUp from "./components/signup/SignUp";
 import Account from "./components/manageAccounts/Account";
 import Termination from "./components/termination/termination";
 import ApproveFiles from "./components/approveFiles/ApproveFiles";
+import { UserContext } from "./userContext/userContext";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
+
   return (
-    <div>
-      <TopNav />
-      <div className="mt-5 pt-3">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/approveFiles" element={<ApproveFiles />} />
-        </Routes>
-      </div>
-    </div>
+    <>
+      <UserContext.Provider value={value}>
+        <TopNav />
+        <div className="mt-5 pt-3">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/approveFiles" element={<ApproveFiles />} />
+          </Routes>
+        </div>
+      </UserContext.Provider>
+    </>
   );
 }
 
