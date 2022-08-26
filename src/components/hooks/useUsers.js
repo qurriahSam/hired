06b007a076e52c6c04a1ab8dc93e11ca3a28f9c3
qurriahsam <mugirethe1@gmail.com>
2022-08-26 21:58:37 +0300

    create fetch call for login

--- src/components/hooks/useUsers.js
+++ src/components/hooks/useUsers.js
@@ -33,3 +33,22 @@ export const signupEmployer = async (employerObj) => {
     console.log("error", error);
   }
 };
+
+export const signin = async (userObj) => {
+  try {
+    const url = "http://localhost:3000/login";
+    const response = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(userObj),
+    });
+    const data = response.json();
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
