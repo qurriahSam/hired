48e5b5b46623294d5e1d4e77a4de83152c51809b
qurriahsam <mugirethe1@gmail.com>
2022-08-28 15:58:00 +0300

    create container to render employee cards

--- src/components/termination/termination.jsx
+++ src/components/termination/termination.jsx
@@ -1,39 +1,19 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import girlReviewImg from "../../images/ladyreview.jpg"
+import React from "react";
+import EmployeeCard from "./EmployeeCard";
 
 function Termination() {
-    return ( 
-      <>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-          <div className="d-md-flex">
-          <Card className="mt-4 p-3 text-right mb-3 me-md-3 " style={{ width: "18rem" }}>
-    <Card.Img
-      variant="top"
-      src={girlReviewImg}
-      className="rounded mx-auto"
-      style={{ width: "6rem" }}
-      />
-      <Card.Body>
-        <Card.Title>Jobseeker Name</Card.Title>
-        <Card.Text>
-         <p>Email@gmail.com</p> 
-         <p>Position:</p> 
-          <p>Employment status:</p>
-
-        </Card.Text>
-        <Button variant="danger">Terminate</Button>{' '}
-      </Card.Body>
-    </Card>
-    </div>
-    </div>
-    </div>
-    </div>
-    
+  return (
+    <>
+      <div className="container mt-2">
+        <div class="row row-cols-auto">
+          <EmployeeCard />
+          <EmployeeCard />
+          <EmployeeCard />
+          <EmployeeCard />
+          <EmployeeCard />
+        </div>
+      </div>
     </>
   );
-};
+}
 export default Termination;
