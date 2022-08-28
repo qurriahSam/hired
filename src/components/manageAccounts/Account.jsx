03c312208347e432ee3d84baf82504a0011d8177
qurriahsam <mugirethe1@gmail.com>
2022-08-28 17:15:45 +0300

    reduce margin and lint file

--- src/components/manageAccounts/Account.jsx
+++ src/components/manageAccounts/Account.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 import Jobseekers from "./jobseekers";
-import Employers from "./employer"
+import Employers from "./employer";
 
 function ManageAccount() {
   const [selected, setSelected] = React.useState(true);
@@ -9,29 +9,28 @@ function ManageAccount() {
   };
 
   return (
-    <div className="container mt-5 pt-3 text-center">
+    <div className="container mt-2">
       <div className="">
-
-      <div>
-        <h2 className="fw-bold">Manage Account</h2>
-        <div className="d-flex fw-bold fs-5">
-          <p
-            style={{ cursor: "pointer", color: selected ? "blue" : "black" }}
-            onClick={handleSelected}
-          >
-            Jobseeker
-          </p>
-          <span className="fw-bold px-2">|</span>
-          <p
-            style={{ cursor: "pointer", color: selected ? "black" : "blue" }}
-            onClick={handleSelected}
-          >
-            Employer
-          </p>
+        <div>
+          <h2 className="fw-bold">Manage Accounts</h2>
+          <div className="d-flex fw-bold fs-5">
+            <p
+              style={{ cursor: "pointer", color: selected ? "blue" : "black" }}
+              onClick={handleSelected}
+            >
+              Jobseeker
+            </p>
+            <span className="fw-bold px-2">|</span>
+            <p
+              style={{ cursor: "pointer", color: selected ? "black" : "blue" }}
+              onClick={handleSelected}
+            >
+              Employer
+            </p>
+          </div>
+          {selected ? <Jobseekers /> : <Employers />}
         </div>
-        {selected ? <Jobseekers /> : <Employers />}
       </div>
-    </div>
     </div>
   );
 }
