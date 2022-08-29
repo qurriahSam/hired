9a214a335b2e3d80ca7f1d3ce8ece9fd89ff1ed2
qurriahsam <mugirethe1@gmail.com>
2022-08-29 11:46:39 +0300

    fetch function to get jobseekers

--- src/components/manageAccounts/jobseekers.jsx
+++ src/components/manageAccounts/jobseekers.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DeleteEmployeeCard from "./DeleteEmployeeCard";
 
 function Jobseekers() {
+  const [jobseekers, setJobSeekers] = useState([]);
+
+  useEffect(() => {
+    const employeeFetch = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/job_seekers`);
+        const data = await response.json();
+        setJobSeekers(data);
+        console.log(jobseekers);
+      } catch (error) {
+        console.log("fetch songs error", error);
+      }
+    };
+    employeeFetch();
+  });
+
   return (
     <div className="container">
       <div className="row row-cols-auto">
