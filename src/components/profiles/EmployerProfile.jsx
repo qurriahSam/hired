6c8d7edb36ab64e5e63d18fecb1691fd01b91d2c
qurriahsam <mugirethe1@gmail.com>
2022-09-02 04:08:35 +0300

    add modals for editing employer profile

--- src/components/profiles/EmployerProfile.jsx
+++ src/components/profiles/EmployerProfile.jsx
@@ -1,33 +1,41 @@
-import React, { useContext } from "react";
-import girlReviewImg from "../../images/ladyreview.jpg";
+import React, { useContext, useState } from "react";
+import noImg from "../../images/placeholderimage.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 import { UserContext } from "../../userContext/userContext";
+import EmployerProfilePicSectionModal from "./EmployerProfilePicSectionModal";
+import EmployerAboutModal from "./EmployerAboutModal";
 
 const EmployerProfile = () => {
-  const { user, setUser } = useContext(UserContext);
+  const [profilePicModalShow, setProfilePicModalShow] = useState(false);
+  const [employerAboutModalShow, setEmployerAboutModalShow] = useState(false);
+  const { user } = useContext(UserContext);
 
   return (
     <div className="container mt-3" style={{ maxWidth: "40em" }}>
       <Card className="shadow-sm">
         <span className="d-flex justify-content-end pe-4 pt-3">
-          <FontAwesomeIcon icon={faPencil} color="gray" />
+          <FontAwesomeIcon
+            icon={faPencil}
+            color="gray"
+            onClick={() => setProfilePicModalShow(true)}
+          />
         </span>
         <Card.Body className="d-md-flex justify-content-around">
           <div>
             <Card.Img
               variant="top"
-              src={girlReviewImg}
+              src={noImg}
               className="rounded-circle mx-auto"
               style={{ maxWidth: "10em" }}
             ></Card.Img>
             <Card.Title className="fw-bold pt-2">
-              {user.first_name} {user.last_name}
+              {user?.first_name} {user?.last_name}
             </Card.Title>
             <Card.Subtitle className="text-secondary fw-bold fs-6">
-              {user.company_name}
+              {user?.company_name}
             </Card.Subtitle>
           </div>
           <div>
@@ -35,23 +43,23 @@ const EmployerProfile = () => {
               <tbody>
                 <tr>
                   <td>Username:</td>
-                  <td>{user.user_name}</td>
+                  <td>{user?.user_name}</td>
                 </tr>
                 <tr>
                   <td>Email:</td>
-                  <td>{user.email}</td>
+                  <td>{user?.email}</td>
                 </tr>
                 <tr>
                   <td>Phone:</td>
-                  <td>{user.phone_number ? user.phone_number : "not set"}</td>
+                  <td>{user?.phone_number ? user.phone_number : "not set"}</td>
                 </tr>
                 <tr>
                   <td>Subscription:</td>
-                  <td>{user.subscription ? "active" : "not subscribed"}</td>
+                  <td>{user?.subscription ? "active" : "not subscribed"}</td>
                 </tr>
                 <tr>
                   <td>Employees:</td>
-                  <td>{user.job_seekers.length}</td>
+                  <td>{user?.job_seekers.length}</td>
                 </tr>
               </tbody>
             </Table>
@@ -63,20 +71,27 @@ const EmployerProfile = () => {
         <Card.Body>
           <span className="d-flex justify-content-between">
             <Card.Title className="fw-bold fs-4">About Me</Card.Title>
-            <FontAwesomeIcon icon={faPencil} color="gray" />
+            <FontAwesomeIcon
+              icon={faPencil}
+              color="gray"
+              style={{ cursor: "pointer" }}
+              onClick={() => setEmployerAboutModalShow(true)}
+            />
           </span>
           <Card.Text>
-            I would describe myself my self now in four different parts. I would describe myself as
-            a kind, generous and helpful person. I love to help my friends out by giving them advice
-            with their problems, and by having their back whenever they need it from supporting them
-            in activities such as going to band concerts to being there emotionally when a break up
-            happens. I would also describe myself as a very artistic, imaginative and creative. I
-            love to build and make things with my hands, especially in art. For example, I enjoy
-            sculpting and creating tiny tiny creatures with clay. Thirdly, I would describe myself
-            as a very intelligent, curious and wondrous person.
+            {user?.employer ? user?.employer : "add brief description about yourself"}
           </Card.Text>
         </Card.Body>
       </Card>
+
+      <EmployerProfilePicSectionModal
+        show={profilePicModalShow}
+        onHide={() => setProfilePicModalShow(false)}
+      />
+      <EmployerAboutModal
+        show={employerAboutModalShow}
+        onHide={() => setEmployerAboutModalShow(false)}
+      />
     </div>
   );
 };
